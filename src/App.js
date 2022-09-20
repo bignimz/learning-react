2d8f0c45a32102836c8ace978b1fe02720028c41
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-09-20 11:06:13 +0300

    create first react component, employee component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Employee from "./components/employee";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +16,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <>
+        <Employee name="John" role="Senior Dev" />
+        <Employee />
+        <Employee />
+      </>
     </div>
   );
 }
---
+++ src/components/employee.js
@@ -0,0 +1,5 @@
+function Employee() {
+  return <h1>Employee appears here</h1>;
+}
+
+export default Employee;
